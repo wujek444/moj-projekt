9ce603a01290c1790888754481050bdc5dddb917
Jakub Wójcik <wujek444@gmail.com>
2018-02-19 23:27:45 +0100

    2 - The Basics - Working with the Component Selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //tutaj deklarujemy nowe komponenty
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //tutaj dodajemy nowe moduły do aplikacji
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //tutaj ładujemy coś podczas startu aplikacji
 })
 export class AppModule { }
