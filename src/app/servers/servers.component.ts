de4e97da5eb04420de6b37b81a75774523bfe6d1
Jakub Wójcik <wujek444@gmail.com>
2018-02-20 22:05:57 +0100

    2 - The Basics - Working with the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers', //[] -> atrybut; brak -> element; można po klasach; nie można po idkach; hovery nie działają
   template: `
     <app-server></app-server>
     <app-server></app-server>
